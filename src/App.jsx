6782029b2f07760c3aa6743e676ff949d53541ba
kratus10 <justineforever@ymail.com>
2024-05-07 17:31:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Mynav from './Navigation/Mynav.jsx'
+import './pages/index.css'
+
+function App() {
+  return (
+    <>
+      <Mynav/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navigation/Mynav.jsx
@@ -0,0 +1,11 @@
+import { FaGithub } from 'react-icons/fa';
+
+function Mynav() {
+    return (
+        <div className="nav-comp" >
+             
+                <h1><FaGithub className="github-icon"/><span className='jay'>List of Justine's GitHub Repos</span></h1>
+        </div>
+    )
+}
+export default Mynav
